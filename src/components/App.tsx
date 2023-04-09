e037a1cd583086d29e8b1d42da3111b212dfbf36
Cypphex <cypphex@outlook.com>
2023-04-09 04:49:11 -0400

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './App.module.css'
+
+const App = () => {
+    return (
+        <div className={styles.app}>
+            <h1>
+                <a href="http://cypphex.com">cypphex.com</a>
+            </h1>
+        </div>
+    )
+}
+
+export default App
